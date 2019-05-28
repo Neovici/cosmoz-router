cd6ca26b109b7ec8c6d673cd89c2fa417b211394
Meghea Iulian <iulian.meghea@gmail.com>
2019-05-28 20:35:09 +0300

    Handles empty templateId

--- cosmoz-page-router.js
+++ cosmoz-page-router.js
@@ -433,7 +433,7 @@
 					return;
 				}
 				//NOTE: when polyfilled importLink.import is not a Document but querySelector is available
-				const template = importLink.import.querySelector('#' + route.templateId);
+				const template = route.templateId && importLink.import.querySelector('#' + route.templateId);
 
 				if (!template) {
 					this._fireEvent('template-not-found', eventDetail);
