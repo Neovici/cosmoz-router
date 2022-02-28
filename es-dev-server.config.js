462ec49f19ee84807f4d2d158f422d30c1af2496
Meghea Iulian <iulian.meghea@gmail.com>
2022-02-28 19:12:34 +0200

    feat!: drop cosmoz-page-location
    BREAKING CHANGE: remove deprecated cosmoz-page-location

--- es-dev-server.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-module.exports = {
-	appIndex: 'demo/index.html',
-	open: true,
-	preserveSymlinks: true,
-	dedupe: true,
-	nodeResolve: { mainFields: ['browser', 'jsnext', 'jsnext:main', 'module', 'main']}
-};
