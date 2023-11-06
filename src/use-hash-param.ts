c62226e1ccabb4d225f8185c6d6e37a1463ce1ab
Meghea Iulian <iulian.meghea@gmail.com>
2023-11-06 20:18:30 +0200

    feat(use-hash-param): adjust return type

--- src/use-hash-param.ts
+++ src/use-hash-param.ts
@@ -3,7 +3,7 @@ import { useState, useEffect, useMemo, useRef } from 'haunted';
 const hashUrl = () =>
 		new URL(
 			location.hash.replace(/^#!?/iu, '').replace('%23', '#'),
-			location.origin
+			location.origin,
 		),
 	parameterize = (hashParam?: string) =>
 		hashParam
@@ -12,60 +12,61 @@ const hashUrl = () =>
 			: undefined;
 
 export const link = (hashParam: string, value?: string | null) => {
-		if (!hashParam) {
+	if (!hashParam) {
+		return;
+	}
+	const url = hashUrl(),
+		sp = new URLSearchParams(url.hash.replace('#', ''));
+
+	if (value == null) {
+		sp.delete(hashParam);
+	} else {
+		sp.set(hashParam, value);
+	}
+
+	return (
+		'#!' + Object.assign(url, { hash: sp }).href.replace(location.origin, '')
+	);
+};
+
+export const useHashParam = (hashParam?: string) => {
+	const parameterized = useMemo(() => parameterize(hashParam), [hashParam]),
+		[param, setParam] = useState(parameterized),
+		ref = useRef(param);
+
+	// eslint-disable-next-line no-void
+	useEffect(() => void (ref.current = param), [param]);
+
+	useEffect(() => {
+		if (parameterized == null) {
 			return;
 		}
-		const url = hashUrl(),
-			sp = new URLSearchParams(url.hash.replace('#', ''));
-
-		if (value == null) {
-			sp.delete(hashParam);
-		} else {
-			sp.set(hashParam, value);
-		}
-
-		return (
-			'#!' + Object.assign(url, { hash: sp }).href.replace(location.origin, '')
-		);
-	},
-	useHashParam = (hashParam?: string) => {
-		const parameterized = useMemo(() => parameterize(hashParam), [hashParam]),
-			[param, setParam] = useState(parameterized),
-			ref = useRef(param);
-
-		// eslint-disable-next-line no-void
-		useEffect(() => void (ref.current = param), [param]);
-
-		useEffect(() => {
-			if (parameterized == null) {
+		const readParam = () => {
+			const newParam = parameterized();
+			if (ref.current === newParam) {
 				return;
 			}
-			const readParam = () => {
-				const newParam = parameterized();
-				if (ref.current === newParam) {
-					return;
-				}
-				setParam(newParam);
-			};
-			readParam();
-			window.addEventListener('popstate', readParam);
-			window.addEventListener('hashchange', readParam);
-			return () => {
-				window.removeEventListener('popstate', readParam);
-				window.removeEventListener('hashchange', readParam);
-			};
-		}, [parameterized]);
+			setParam(newParam);
+		};
+		readParam();
+		window.addEventListener('popstate', readParam);
+		window.addEventListener('hashchange', readParam);
+		return () => {
+			window.removeEventListener('popstate', readParam);
+			window.removeEventListener('hashchange', readParam);
+		};
+	}, [parameterized]);
 
-		const set = useMemo(
-			() =>
-				hashParam
-					? (v: string | null) => {
-							setParam(v);
-							history.pushState({}, '', link(hashParam, v));
-					  }
-					: setParam,
-			[hashParam]
-		);
+	const set = useMemo(
+		() =>
+			hashParam
+				? (v: string | null) => {
+						setParam(v);
+						history.pushState({}, '', link(hashParam, v));
+				  }
+				: setParam,
+		[hashParam],
+	);
 
-		return [param, set];
-	};
+	return [param, set] as const;
+};
