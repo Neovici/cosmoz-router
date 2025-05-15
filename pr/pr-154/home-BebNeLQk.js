8d4bee2f678d74bbd62b2e412a9c8a26bf16ad8d
basilgood <basilgood@users.noreply.github.com>
2025-05-15 07:44:01 +0000

    Remove preview for PR 154 🛬

--- pr/pr-154/home-BebNeLQk.js
+++
@@ -1,7 +0,0 @@
-import { c as component } from './cosmoz-router.stories-hUaHBjqv.js';
-import { x } from './directive-helpers-B3SPnMX6.js';
-
-customElements.define(
-  "demo-home",
-  component(() => x` <h2>Welcome to the home page</h2> `)
-);
--- pr/pr-154/preview-DQG4A6rV.js
+++
@@ -1,3 +0,0 @@
-var parameters={docs:{renderer:async()=>{let{DocsRenderer}=await import('./WithTooltip-V3YHNWJZ-hzwBXB-5.js').then(function (n) { return n.D; });return new DocsRenderer}}};
-
-export { parameters };
