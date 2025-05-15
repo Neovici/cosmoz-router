c03d23a30b0b381113099c42817e099058cfb3a5
basilgood <elsile69@yahoo.com>
2025-05-15 10:28:12 +0300

    ci: add storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,20 @@
-module.exports = {
-  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+import esbuild from 'rollup-plugin-esbuild';
+
+/** @type { import('@web/storybook-framework-web-components').StorybookConfig } */
+const config = {
+	stories: ['../stories/**/*.stories.{js,ts,mdx}'],
+	addons: ['@storybook/addon-essentials', '@storybook/addon-links'],
+	framework: {
+		name: '@web/storybook-framework-web-components',
+	},
+	/* Try to make the build parse TS files */
+	async rollupFinal(config) {
+		// add extra configuration for rollup
+		// e.g. a new plugin
+		config.plugins.push(esbuild({}));
+
+		return config;
+	},
 };
+
+export default config;
