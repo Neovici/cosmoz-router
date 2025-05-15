2199371ba8e03ba8ebc667f8b319ac66e5e81a27
basilgood <basilgood@users.noreply.github.com>
2025-05-15 07:39:37 +0000

    Deploy preview for PR 158 🛫

---
+++ pr/pr-158/inline-module-a412f23235eb971c4a27c3950e98fd49.js
@@ -0,0 +1,37 @@
+import './sb-preview/runtime.js';
+
+const channel = __STORYBOOK_MODULE_CHANNEL_POSTMESSAGE__.createChannel({ page: "preview" });
+__STORYBOOK_MODULE_PREVIEW_API__.addons.setChannel(channel);
+window.__STORYBOOK_ADDONS_CHANNEL__ = channel;
+const { SERVER_CHANNEL_URL } = globalThis;
+if (SERVER_CHANNEL_URL) {
+  const serverChannel = __STORYBOOK_MODULE_CHANNEL_WEBSOCKET__.createChannel({ url: SERVER_CHANNEL_URL });
+  __STORYBOOK_MODULE_PREVIEW_API__.addons.setServerChannel(serverChannel);
+  window.__STORYBOOK_SERVER_CHANNEL__ = serverChannel;
+}
+
+const importers = {
+  "./stories/cosmoz-router.stories.js": () => import('./cosmoz-router.stories-hUaHBjqv.js').then(function (n) { return n.b; })
+};
+function importFn(path) {
+  return importers[path]();
+}
+
+const getProjectAnnotations = async () => {
+  const configs = await Promise.all([
+    import('./entry-preview-CBwohQTD.js'),
+    import('./entry-preview-docs-_d8H52g1.js'),
+    import('./preview-DQG4A6rV.js'),
+    import('./preview-BcJVSA4a.js'),
+    import('./preview-Bf0d1tVO.js'),
+    import('./preview-CU2JNqMw.js'),
+    import('./preview-BT9ta7nr.js'),
+    import('./preview-CDagOaLU.js'),
+    import('./preview-B8oLrdpj.js')
+  ]);
+  return __STORYBOOK_MODULE_PREVIEW_API__.composeConfigs(configs);
+};
+window.__STORYBOOK_PREVIEW__ = window.__STORYBOOK_PREVIEW__ || new __STORYBOOK_MODULE_PREVIEW_API__.PreviewWeb();
+window.__STORYBOOK_STORY_STORE__ = window.__STORYBOOK_STORY_STORE__ || window.__STORYBOOK_PREVIEW__.storyStore;
+window.__STORYBOOK_CLIENT_API__ = window.__STORYBOOK_CLIENT_API__ || new __STORYBOOK_MODULE_PREVIEW_API__.ClientApi({ storyStore: window.__STORYBOOK_PREVIEW__.storyStore });
+window.__STORYBOOK_PREVIEW__.initialize({ importFn, getProjectAnnotations });
